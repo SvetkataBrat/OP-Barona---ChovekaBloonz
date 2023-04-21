9fc2483b2e07e2c4422fe15533f52ea01b5fdad5
Qtaro Barona Mineral <92295683+LachezarMitev@users.noreply.github.com>
2023-04-21 15:13:11 +0300

    Add files via upload

---
+++ Auto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buisiness_Layer
+{
+    public class Auto
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string brand { get; set; }
+
+        public double price { get; set; }
+
+        public int horsepower { get; set; }
+
+        [ForeignKey]
+        public int salonID { get; set; }
+
+        [ForeignKey]
+        public int customerID { get; set; }
+
+        private Auto()
+        {
+
+        }
+
+        public Auto(int id, string brand, double price, int horsepower, int salonID, int customerID)
+        {
+            this.id = id;
+            this.brand = brand;
+            this.price = price;
+            this.horsepower = horsepower;
+            this.salonID = salonID;
+            this.customerID = customerID;
+        }
+    }
+}
