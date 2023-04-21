cbf0c2e727633e198ec5b03a7095fba3f7a620c2
icko <57589158+itcaka50@users.noreply.github.com>
2023-04-21 15:19:02 +0300

    Add files via upload

--- Buisiness Layer/AutoSalon.cs
+++ Buisiness Layer/AutoSalon.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,31 @@ namespace Buisiness_Layer
 {
     public class AutoSalon
     {
-        //barona
+        [Key]
+        public int SalonId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Adress { get; set; }
+
+        public List<Auto> Cars { get; set; }
+
+        private AutoSalon() 
+        {
+            Cars = new List<Auto>();
+        }
+
+        public AutoSalon(int id, string name, string adress)
+        {
+            id = SalonId;
+            Name = name;
+            Adress = adress;
+        }
+
+
+
+
     }
 }
